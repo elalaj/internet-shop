f7bd04f80748e4d2484d54474a1f990ec5f9b652
endritlalaj <endrit.lalaj@valtech-mobility.com>
2021-06-05 14:59:54 +0200

    init commit

--- src/test/java/bussines/ShoppingCartTest.java
+++ src/test/java/bussines/ShoppingCartTest.java
@@ -1,10 +1,8 @@
 package bussines;
 
 import java.util.Map;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import entity.Product;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
